67cb99505cf616ecdb784a46209a907baa456681
Daniel P. Doyle <thedanieldoyle@outlook.com>
2019-12-04 16:38:40 +0000

    Added non-generic base interface for IEventDispatcherHandler

--- Source/EventDispatcher/IEventDispatchHandler.cs
+++ Source/EventDispatcher/IEventDispatchHandler.cs
@@ -3,11 +3,15 @@ using System.Threading.Tasks;
 
 namespace EventDispatcher
 {
-    public interface IEventDispatchHandler<in TEvent>
+    public interface IEventDispatchHandler<in TEvent> : IEventDispatcherHandler
         where TEvent : IEvent
     {
         void Handle(TEvent @event);
 
         Task HandleAsync(TEvent @event, CancellationToken cancellation = default(CancellationToken));
+    }
+
+    public interface IEventDispatcherHandler
+    {
     }
 }
